988a5d57610c1b07e5dea7851eb96f87b68f6010
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-06-06 22:53:44 -0600

    Template form has been finished with the final topic form states

--- projects/forms/src/app/template-form/template-form.component.ts
+++ projects/forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -6,17 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent {
+  
+  //By adding !, you are telling the compiler that you trust the myForm property to be initialized elsewhere
+  @ViewChild('formContact') myForm!: NgForm;
 
   person = {
     name: "",
     age: ""
   };
 
+
   processForm () {
     console.log(this.person)
     //Cleaning the form input after sending the info
     this.person.name = "";
     this.person.age = "";
+
+    // Resets all the properties from the form, including inputs
+    this.myForm.reset();
 
   };
 
