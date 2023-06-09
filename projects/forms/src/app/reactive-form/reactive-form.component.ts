0ac57e8d45f25404d338228f2d2da4e22e098449
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-06-08 23:55:46 -0600

    Form Group property has been added to the reactive form

--- projects/forms/src/app/reactive-form/reactive-form.component.ts
+++ projects/forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 //Import "FormControl" to work with reactive forms
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, NgForm, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,8 +9,33 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent {
 
-  //Need to create an Instance from the FormControl Class
+  /* //Need to create an Instance from the FormControl Class
   //In the input we need to write [formControl]="name"
   name = new FormControl("", Validators.required);
-  email = new FormControl("", [ Validators.required, Validators.email ] );
+  email = new FormControl("", [ Validators.required, Validators.email ] ); */
+
+  //By adding !, you are telling the compiler that you trust the myForm property to be initialized elsewhere
+  @ViewChild('formContact') myForm!: NgForm;
+
+  get nameGetter () {
+    //WE add "as FormControl" to don't write the "?" in the HTML file
+    return this.formUser.get("name") as FormControl;
+  }
+
+  get emailGetter () {
+    return this.formUser.get("email");
+  }
+
+  //Identify the form
+  formUser = new FormGroup ({
+    "name": new FormControl("", Validators.required),
+    "email": new FormControl("", [Validators.required, Validators.email])
+  });
+
+  processInfo () {
+    console.log(this.formUser.value);
+
+    // Resets all the properties from the form, including inputs
+    this.myForm.reset();
+  };
 }
