d5168eff6f33762d3428ce02c4864063b2508f5a
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-06-20 21:50:12 -0600

    Output from children component to parent component has been added

--- projects/basic-concepts/src/app/children/children.component.ts
+++ projects/basic-concepts/src/app/children/children.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-children',
@@ -13,6 +13,13 @@ export class ChildrenComponent {
   @Input() dataFromTypescript?: String;
   @Input() dataFromTypescript2?: String;
 
-  @Input() dataFromInput: String = "";
+  @Input() dataFromInput: string = "";
 
+  // To create a new Event we need to import "EventEmitter" from @angular/core
+  // To transmit our Event we need to import "Output" from @angular/core
+  @Output() dataFromInputChange = new EventEmitter<string>();
+
+  emitTitleChange(){
+    this.dataFromInputChange.emit(this.dataFromInput);
+  }
 }
