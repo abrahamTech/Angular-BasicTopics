a7f2cdf76ddba65d406e0fcb83ee6a96ac6596d9
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-08-28 21:22:22 -0600

    feat(Modules): add lazy loading for the Courses and principal route paths in the app routing module

--- projects/modules/src/app/app-routing.module.ts
+++ projects/modules/src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: "",
+    loadChildren: () => import("./welcome/welcome.module").then(m => m.WelcomeModule)
+  },
+  {
+    path: "courses",
+    loadChildren:() => import("./courses/courses.module").then(m => m.CoursesModule)
+  }
 ];
 
 @NgModule({
